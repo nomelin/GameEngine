1ea1df374c8d1eed8dc9992aa24b99a845271e61
nomelin <2023686595@qq.com>
2023-05-14 23:15:20 +0800

    demo1

--- src/main/java/top/nomelin/engine/entity/Entity.java
+++ src/main/java/top/nomelin/engine/entity/Entity.java
@@ -5,35 +5,37 @@ import top.nomelin.engine.component.Component;
 import top.nomelin.engine.interfaces.EntityInterface;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Objects;
 import java.util.Vector;
 
-import static java.lang.Class.forName;
 
 public abstract class Entity implements EntityInterface {
     /**
      * 实体对应的组件列表
      */
     private Vector<Component> components;
+    //是否处于start()-stop()
+    private boolean onStart;
+
     // 构造方法，初始化组件列表为空
     Entity() {
+        super();
         components = new Vector<>();
     }
 
     /**
-     *  添加组件的方法，传入一个组件类名作为参数，并将其加入到组件列表中
-     *  */
+     * 添加组件的方法，传入一个组件类名作为参数，并将其加入到组件列表中
+     */
     public boolean addComponent(String name) {
         try {
             //使用Class.forName(name)方法来获取Class对象
             Class<?> componentClass = Class.forName(name);
             //getDeclaredConstructor()方法会根据他的参数对该类的构造函数进行搜索并返回对应的构造函数，
             // 没有参数就返回该类的无参构造函数，然后再通过newInstance进行实例化。
-            Object object=componentClass.getDeclaredConstructor().newInstance();
-            if(object instanceof Component component){
+            Object object = componentClass.getDeclaredConstructor().newInstance();
+            if (object instanceof Component component) {
                 components.add(component);
                 return true;
-            }else {
+            } else {
                 return false;
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException |
@@ -42,14 +44,15 @@ public abstract class Entity implements EntityInterface {
         }
 
     }
-    public boolean deleteComponent(String name){
+
+    public boolean deleteComponent(String name) {
         try {
             Class<?> componentClass = Class.forName(name);
-            Object object=componentClass.getDeclaredConstructor().newInstance();
-            if(object instanceof Component component){
+            Object object = componentClass.getDeclaredConstructor().newInstance();
+            if (object instanceof Component component) {
                 components.remove(component);
                 return true;
-            }else {
+            } else {
                 return false;
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException |
@@ -57,42 +60,61 @@ public abstract class Entity implements EntityInterface {
             throw new RuntimeException(e);
         }
     }
-    public void init(){
+
+    public void init() {
 
     }
 
     @Override
     public void start() {
-
+        onStart = true;
     }
 
     @Override
-    public void fixedUpdate(){
-
-    }
-    // 更新实体的方法，遍历组件列表，并调用每个组件的更新方法
-    public void update() {
-        for (Component component : components) {
-            component.update();
+    public boolean fixedUpdate() {
+        if (!onStart) {
+            return false;
         }
+        //TODO
+        return true;
     }
 
+    // 更新实体的方法，遍历组件列表，并调用每个组件的更新方法
     @Override
-    public void Update() {
-
+    public boolean update() {
+        if (!onStart) {
+            return false;
+        }
+        for (Component component : components) {
+            component.update();//TODO 不是所有组件都在这个阶段调用
+        }
+        return true;
     }
 
-    public void lateUpdate(){
-
+    public boolean lateUpdate() {
+        if (!onStart) {
+            return false;
+        }
+        //TODO
+        return true;
     }
+
 
     @Override
     public void stop() {
-
+        onStart = false;
     }
 
     @Override
     public void destroy() {
+        components.clear();
+        components = null;
+    }
 
+    /**
+     * 是否处于start状态
+     */
+    public boolean OnStart() {
+        return onStart;
     }
 }
--- src/main/java/top/nomelin/engine/interfaces/EntityInterface.java
+++ src/main/java/top/nomelin/engine/interfaces/EntityInterface.java
@@ -3,9 +3,9 @@ package top.nomelin.engine.interfaces;
 public interface EntityInterface {
     void init();
     void start();
-    void fixedUpdate();
-    void Update();
-    void lateUpdate();
+    boolean fixedUpdate();
+    boolean update();
+    boolean lateUpdate();
     void stop();
     void destroy();
 }
