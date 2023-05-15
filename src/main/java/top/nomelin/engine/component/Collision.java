b96014da7c94464cda3ee6267140fdc1eb6346f2
nomelin <2023686595@qq.com>
2023-05-15 21:19:33 +0800

    demo1

---
+++ src/main/java/top/nomelin/engine/component/Collision.java
@@ -0,0 +1,19 @@
+package top.nomelin.engine.component;
+
+import top.nomelin.engine.entity.Entity;
+
+public class Collision extends Component{
+    Collision(Entity entity) {
+        super(entity);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public Entity getEntity() {
+        return super.getEntity();
+    }
+}
